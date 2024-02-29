289945b323ab462dbf431cfd8ef73e28aff089aa
SmithC <02Cade.Smith@gulllakecs.org>
2024-02-29 07:58:13 -0500

    Setting up AI.py program

---
+++ AI_Playground.py
@@ -0,0 +1,5 @@
+# Programmer - Cade Smith
+# Date - 2.29.2024
+# Program - AI Playground
+
+print("This will be a place for me to play with programming using Python programming using AI Technology.")
